1cca3f8334553b85fab05bb0b434084594397bc4
Erick Ortiz <eo@ceyeber.co>
2024-07-21 02:59:08 -0700

    Fixed docker compose issue via restarting backend

--- database/init.sql
+++ database/init.sql
@@ -1,5 +1,5 @@
--- Create items table
-CREATE TABLE items (
+-- Create the items table in the shopping_list database
+CREATE TABLE IF NOT EXISTS items (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     description VARCHAR(100),
